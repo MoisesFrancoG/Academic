e9a6725ff89791db2e8ce5d2b63ec12a50ec403c
miguel.molina <233371@ids.upchiapas.edu.mx>
2025-12-02 22:04:04 -0600

    Add CORS for localhost:5173

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
 
   // Configuración de CORS
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://academicx.chuy7x.space/'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     credentials: true,
   });
