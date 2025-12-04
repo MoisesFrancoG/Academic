4700739b775e8dc1cbc3f7d132448d1f935d75d3
miguel.molina <233371@ids.upchiapas.edu.mx>
2025-12-04 02:53:32 -0600

    fix user deletion cascade

--- src/alumno/alumno.module.ts
+++ src/alumno/alumno.module.ts
@@ -2,14 +2,17 @@ import { Module } from '@nestjs/common';
 import { AlumnoControllerModule } from './controller/controller.module';
 import { AlumnoServiceModule } from './service/service.module';
 import { AlumnoRepositoryModule } from './repository/repository.module';
+import { InscripcionGrupoRepositoryModule } from '../inscripciones-grupo/repository/repository.module';
 
 /**
  * Módulo principal de Alumno
  * Agrupa todos los submódulos relacionados con la entidad Alumno
+ * Importa InscripcionGrupoRepositoryModule para soportar el cascade delete
  */
 @Module({
   imports: [
     AlumnoRepositoryModule,
+    InscripcionGrupoRepositoryModule,
     AlumnoServiceModule,
     AlumnoControllerModule,
   ],
